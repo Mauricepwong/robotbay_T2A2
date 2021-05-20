5b23efa7d0fbc633036d5a206f27db085498c13d
Maurice Wong <cas0121202@coderacademy.edu.aulearclearlsgit initgit config --global usegit config --global user.email cas0121202@coderacademy.edu.au>
2021-05-20 19:45:55 +1000

    Create Navbar

--- .history/config/environments/production_20210520173621.rb
+++ .history/config/environments/production_20210520173621.rb
@@ -118,7 +118,8 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
+   # Setup for devise
+   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  # Setup for devise
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
